de06582e0721c4736e3a885f37eac4ea7afb7c68
viratc <virat.viratc@gmail.com>
2020-05-06 17:33:53 +0530

    Build min heapclear

---
+++ Sorting/minHeap.py
@@ -0,0 +1,28 @@
+class MinHeap:
+
+    def __init__(self, array):
+        self.heap = []
+        self.array = array
+
+    def buildHeap(self, array):
+        pass
+
+    def siftDown(self):
+        pass
+
+    def siftUp(self):
+        pass
+
+    def peek(self):
+        return self.heap[0]
+
+    def insert(self, value):
+        pass
+
+    def remove(self, value):
+        pass
+
+    def swap(self, i, j, array):
+        array[i], array[j] = array[j], array[i]
+
+
